ac836da2603b754ba7a7cc583e229a84ecddc11f
Jack <nian.jack@gmail.com>
2019-09-18 20:38:49 -0400

    added multiple db connections

--- bright-ideas/src/db-connection.js
+++ bright-ideas/src/db-connection.js
@@ -3,10 +3,17 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "yourusername",
-  password: "yourpassword"
+  password: "yourpassword",
+  database: "yourdatabase",
+  multipleStatements: true,
+  connectionLimit: 10
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+  if (err) {
+    console.log(err);
+  }
+  else{
+    console.log("Connected!");
+  }
 });
