42bb983e2bf34b0e793750bb87d12234c51d4936
Jack <nian.jack@gmail.com>
2019-09-23 10:09:16 -0400

    fixed user queries

--- bright-ideas/src/app/services/user-queries.js
+++ bright-ideas/src/app/services/user-queries.js
@@ -1,10 +1,26 @@
 var { getDB } = require('./db-connection');
 
-
+// Get all user information
 async function getAllUsers() {
-    getDB().query("select * from user", (err, rows) => {
-       if (!err) {
-         console.log(rows);
-       }
-     });
- }
+  getDB().query("SELECT * FROM user", (err, rows) => {
+    if (!err) {
+      console.log(rows);
+      return rows;
+    }
+    else {
+      console.log("error: " + err);
+    }
+  });
+}
+
+// Get all users by emailAddress column with PARAM: email
+async function getUserByEmail(email) {
+  getDB().query("SELECT * FROM user WHERE emailAddress = ?", [email], (err, rows) => {
+    if (!err) {
+      console.log(rows);
+      return rows;
+    }
+    else
+      console.log("error: " + err);
+  })
+}
