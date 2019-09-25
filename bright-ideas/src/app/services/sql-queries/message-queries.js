b3029b67ce0812b071ec20df0efddcf0619d1ea4
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-25 19:49:39 -0400

    working on queries

---
+++ bright-ideas/src/app/services/sql-queries/message-queries.js
@@ -0,0 +1,56 @@
+module.exports = function() {
+    // Base URL to The back end API
+    let baseUrl = "https://bright-ideas-api.herokuapp.com/"
+
+    return {
+        // Make a GET request to back end API to get all profiles
+        getAllMessages(){
+            return fetch(baseUrl + "messages/", {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                return Promise.resolve(data)
+            })
+            .catch(error => {
+                console.log("Error: " + error)
+            })
+        },
+        getMessageById(messageId){
+            return fetch(baseUrl + "media/" + messageId, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                return Promise.resolve(data)
+            })
+            .catch(error => {
+                console.log("Error: " + error)
+            })
+        },
+        getMessageBySenderId(senderId) {
+            return fetch(baseUrl + "sender/" + senderId, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                return Promise.resolve(data)
+            })
+            .catch(error => {
+                console.log("Error" + error)
+            })
+        }
+    }
+} 
