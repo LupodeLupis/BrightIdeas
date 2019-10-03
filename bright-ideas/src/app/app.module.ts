354fff09a114ee35620dbc3535bd979201a523d0
Jack <nian.jack@gmail.com>
2019-10-03 15:19:24 -0400

    added http to providers

--- bright-ideas/src/app/app.module.ts
+++ bright-ideas/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './modules/layout/header/header.component';
 import { ContentComponent } from './modules/layout/content/content.component';
 import { ChangePasswordComponent } from './modules/auth/component/change-password/change-password.component';
 import { ViewMessageComponent } from './modules/chat/component/view-message/view-message.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ViewMessageComponent } from './modules/chat/component/view-message/view
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
