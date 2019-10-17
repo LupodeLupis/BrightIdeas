2dfdc5b9159a3c18430da84ece7edff88e4dc388
Jack <nian.jack@gmail.com>
2019-10-17 11:44:57 -0400

    added envrioment variable api

--- bright-ideas/src/app/services/profile-endpoint.service.ts
+++ bright-ideas/src/app/services/profile-endpoint.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Profile } from '../models/profile'
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileEndpointService {
 
-  private url = "https://bright-ideas-api.herokuapp.com";
+  private url = environment.api;
 
   constructor(private http: HttpClient) { }
 
   getAllProfiles(): Observable<Profile[]> {
-    return this.http.get<Profile[]>(`${this.url}/profiles`);
+    return this.http.get<Profile[]>(`${this.url}/profile`);
   }
 
   getProfileById(id): Observable<Profile[]> {
