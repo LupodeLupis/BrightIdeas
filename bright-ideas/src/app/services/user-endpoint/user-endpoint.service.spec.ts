edeaee99e7b291cf70a6d1a3ecc77356b790d925
Jack <nian.jack@gmail.com>
2019-10-20 13:03:59 -0400

    accepted merge in user test spec

--- bright-ideas/src/app/services/user-endpoint/user-endpoint.service.spec.ts
+++ bright-ideas/src/app/services/user-endpoint/user-endpoint.service.spec.ts
@@ -1,10 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 
-<<<<<<< HEAD
-import { UserEndpointService } from "../user-endpoint.service";
-=======
 import { UserEndpointService } from '../../services/user-endpoint/user-endpoint.service';
->>>>>>> 327f051ee2ca4c845ad052b143c65a7d0aa7b9ac
 
 describe("UserEndpointService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
