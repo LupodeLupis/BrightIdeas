eeab8dfc90b59d941d9a870e12a0838963e8eff2
Jack <nian.jack@gmail.com>
2019-10-20 17:59:33 -0400

    added create update delete

--- bright-ideas/src/app/services/idea-endpoint/idea-endpoint.service.spec.ts
+++ bright-ideas/src/app/services/idea-endpoint/idea-endpoint.service.spec.ts
@@ -2,9 +2,14 @@ import { TestBed, async } from "@angular/core/testing";
 
 import { IdeaEndpointService } from "./idea-endpoint.service";
 import { HttpClientModule } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../environments/environment'
 
 
 describe("IdeaEndpointService", () => {
+  let httpTestingController: HttpTestingController;
+  let ideaEndpointService: IdeaEndpointService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -17,4 +22,10 @@ describe("IdeaEndpointService", () => {
     const service: IdeaEndpointService = TestBed.get(IdeaEndpointService);
     expect(service).toBeTruthy();
   });
+
+  // it("should get all ideas", () => {
+  //   const testData = {
+
+  //   }
+  // })
 });
