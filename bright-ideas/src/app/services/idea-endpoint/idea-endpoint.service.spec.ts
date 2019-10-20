cf90e13d81c40aa9db4fdbd145c2de1142f67c0a
Jack <nian.jack@gmail.com>
2019-10-20 13:44:32 -0400

    fixed missing imports from tests

--- bright-ideas/src/app/services/idea-endpoint/idea-endpoint.service.spec.ts
+++ bright-ideas/src/app/services/idea-endpoint/idea-endpoint.service.spec.ts
@@ -1,9 +1,17 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, async } from "@angular/core/testing";
 
 import { IdeaEndpointService } from "./idea-endpoint.service";
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe("IdeaEndpointService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    })
+  }));
 
   it("should be created", () => {
     const service: IdeaEndpointService = TestBed.get(IdeaEndpointService);
