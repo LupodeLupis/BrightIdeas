baf158b249a6a3af65415bce94007655e91e157b
Jayson Sherry <jaysonsherry@hotmail.com>
2019-10-22 10:44:05 -0400

    working on login

--- bright-ideas/src/app/modules/auth/component/login/login.component.ts
+++ bright-ideas/src/app/modules/auth/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router,) { }
+
+  get f() { return this.loginForm.controls; }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        eMail: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]]
+    });
   }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.loginForm.valid){
+    alert('User form is valid!!')
+    } else {
+    alert('User form is not valid!!')
+    }
+}
+
+createAccount(){
+    this.router.navigate(['/createAccount']);
+}
+
+resetPassword(){
+    this.router.navigate(['/resetPassword']);
+}
 
 }
