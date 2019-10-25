473b4d3038f6f97b80b22e11e5572f7a1f9a9fb8
Jack <nian.jack@gmail.com>
2019-10-25 16:42:13 -0400

    added create update delete

--- bright-ideas/src/app/services/profile-endpoint/profile-endpoint.service.ts
+++ bright-ideas/src/app/services/profile-endpoint/profile-endpoint.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
 import { Profile } from '../../models/profile';
 import { environment } from '../../../environments/environment';
 
@@ -16,11 +16,63 @@ private url = environment.api;
 
 constructor(private http: HttpClient) {}
 
-getAllProfiles(): Observable<Profile[]> {
-  return this.http.get<Profile[]>(`${this.url}/profile`);
+getProfiles(): Observable<any> {
+  return Observable.create((observer: Observer<any>) => {
+    this.http.get(`${this.url}/profile`).subscribe((res: any[]) => {
+       observer.next(res);
+       observer.complete();
+    },
+    (error: HttpErrorResponse) => {
+       observer.error(error);
+    });
+ });;
 }
 
-getProfileById(id): Observable<Profile[]> {
-  return this.http.get<Profile[]>(`${this.url}/profile/` + id);
+getProfileById(id): Observable<any> {
+  return Observable.create((observer: Observer<any>) => {
+    this.http.get(`${this.url}/profile/${id}`).subscribe((res: any) => {
+       observer.next(res);
+       observer.complete();
+    },
+    (error: HttpErrorResponse) => {
+       observer.error(error);
+    });
+ });
+}
+
+createProfile(profile): Observable<any> {
+  return Observable.create((observer: Observer<any>) => {
+    this.http.post(`${this.url}/profile/create`, profile).subscribe((response) => {
+      observer.next(response);
+      observer.complete();
+    },
+      (error) => {
+        observer.error(error)
+      });
+  });
+}
+
+deleteProfile(id): Observable<any> {
+  return Observable.create((observer: Observer<any>) => {
+    this.http.delete(`${this.url}/profile/delete/${id}`).subscribe((response) => {
+      observer.next(response);
+      observer.complete();
+    },
+      (error) => {
+        observer.error(error.message);
+      });
+  });
+}
+
+updateProfile(profile): Observable<any> {
+  return Observable.create((observer: Observer<any>) => {
+    this.http.put(`${this.url}/profile/update`, profile).subscribe((response) => {
+      observer.next(response);
+      observer.complete();
+    },
+    (error) => {
+      observer.error(error)
+    });
+  })
 }
 }
