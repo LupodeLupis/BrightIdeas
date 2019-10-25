047b67894e4368e6d0ff7b7c580f37faa11edc00
Jack <nian.jack@gmail.com>
2019-10-25 17:00:01 -0400

    added unit test

--- bright-ideas/src/app/services/todo-endpoint/todo-endpoint.service.spec.ts
+++ bright-ideas/src/app/services/todo-endpoint/todo-endpoint.service.spec.ts
@@ -2,17 +2,55 @@ import { TestBed, async } from "@angular/core/testing";
 
 import { TodoEndpointService } from "./todo-endpoint.service";
 import { HttpClientModule } from '@angular/common/http';
+import { TestObject } from 'protractor/built/driverProviders';
 
 describe("TodoEndpointService", () => {
+  let service: TodoEndpointService
+  let testToDo = [{
+    id: 1,
+    description: "to do 1 test"
+  }, {
+    id: 2,
+    description: "to do 2 test"
+  }
+  ]
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule]
     })
-    .compileComponents();
+      .compileComponents();
+    service = TestBed.get(TodoEndpointService);
   }));
 
   it("should be created", () => {
-    const service: TodoEndpointService = TestBed.get(TodoEndpointService);
     expect(service).toBeTruthy();
+  });
+
+
+  it('should get all toDos', () => {
+    service.getToDos().subscribe(res => {
+      expect(res).toEqual(testToDo);
+    });
+  });
+
+
+  it('should get a todo by id', () => {
+    service.getToDoById(1).subscribe(res => {
+      expect(res).toEqual(testToDo[0]);
+    });
+  });
+
+
+  it("should create a todo", () => {
+    service.createToDo(testToDo[0]).subscribe((res) => {
+      expect(res).toEqual(testToDo[0]);
+    });
+  });
+
+  it("should update a todo", () => {
+    service.updateToDo(testToDo[1]).subscribe((res) => {
+      expect(res).toEqual(testToDo[1]);
+    });
   });
 });
