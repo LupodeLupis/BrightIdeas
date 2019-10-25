f4270d26267b51e0359e22d68bbbdb89b1c7aff2
Jack <nian.jack@gmail.com>
2019-10-24 21:48:04 -0400

    updated models to reflect db

--- bright-ideas/src/app/models/credentials.ts
+++
@@ -1,6 +0,0 @@
-export class Credentials{
-    id: number;
-    userName: string;
-    password: string;
-    previousPasswords: string; // I think this should be a an array of string, but in the documentation is says varchar with 500 length
-}
