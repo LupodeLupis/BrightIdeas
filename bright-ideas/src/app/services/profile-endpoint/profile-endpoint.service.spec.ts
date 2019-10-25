2076aa9fbb233dba95cf3830ba13bddb02621fdf
Jack <nian.jack@gmail.com>
2019-10-25 16:42:27 -0400

    added unit tests

--- bright-ideas/src/app/services/profile-endpoint/profile-endpoint.service.spec.ts
+++ bright-ideas/src/app/services/profile-endpoint/profile-endpoint.service.spec.ts
@@ -4,15 +4,57 @@ import { ProfileEndpointService } from "./profile-endpoint.service";
 import { HttpClientModule } from '@angular/common/http';
 
 describe("ProfileEndpointService", () => {
-  beforeEach(async(() => {
+  let service: ProfileEndpointService;
+  let testProfile = [{
+    picture: "pictureString",
+    displayName: "displayName",
+    projectDescription: "description of project",
+    idea: 1
+  },
+  {
+    picture: "pictureString2",
+    displayName: "displayName2",
+    projectDescription: "description of project",
+    idea: 2
+  }]
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [ProfileEndpointService]
     })
     .compileComponents();
-  }));
+    service = TestBed.get(ProfileEndpointService);
+  });
 
   it("should be created", () => {
-    const service: ProfileEndpointService = TestBed.get(ProfileEndpointService);
     expect(service).toBeTruthy();
+  });
+
+  
+  it('should get all profile', () => {
+    service.getProfiles().subscribe(res => {
+      expect(res).toEqual(testProfile);
+    });
+  });
+
+  
+  it('should get a profile by id', () => {
+    service.getProfileById(1).subscribe(res => {
+      expect(res).toEqual(testProfile[0]);
+    });
+  });
+
+
+  it("should create a profile", () => {
+    service.createProfile(testProfile[0]).subscribe((res) => {
+      expect(res).toEqual(testProfile[0]);
+    });
+  });
+
+  it("should update profile", () => {
+    service.updateProfile(testProfile[1]).subscribe((res) => {
+      expect(res).toEqual(testProfile[1]);
+    });
   });
 });
