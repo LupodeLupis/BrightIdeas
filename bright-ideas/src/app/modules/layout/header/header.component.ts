d487af78b22691d2ae55f6c5feb7ddd8955def8c
jhui19 <jordanhui19@hotmail.com>
2019-11-04 17:22:14 -0500

    Added searchbar and removed default content

--- bright-ideas/src/app/modules/layout/header/header.component.ts
+++ bright-ideas/src/app/modules/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  search(searchText, searchType): void {
+    console.log("submitted");
+    console.log(searchText.value + " " + searchType);
+
+    if (searchText != null)
+    {
+      if(searchType == null || searchType == "ideas")
+      {
+        this.router.navigateByUrl('/searchResults/Ideas/' + searchText.value)
+      }
+      else
+      {
+        this.router.navigateByUrl('/searchResults/Profiles/' + searchText.value)
+      }
+    }
+  }
 }
