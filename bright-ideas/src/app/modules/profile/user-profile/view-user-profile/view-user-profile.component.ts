6343afc8e6077fd891bdf3387020c9ef77e7ac73
LupodeLupis <alaurenzi06@gmail.com>
2019-11-24 07:16:47 -0500

    workigin on edit and view profile

--- bright-ideas/src/app/modules/profile/user-profile/view-user-profile/view-user-profile.component.ts
+++ bright-ideas/src/app/modules/profile/user-profile/view-user-profile/view-user-profile.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SessionStorageService } from '../../../../shared/services/session-storage/session-storage.service';
+import { User } from 'src/app/models/user';
+import { ProfileEndpointService } from '../../../../services/profile-endpoint/profile-endpoint.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-user-profile',
@@ -6,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-user-profile.component.css']
 })
 export class ViewUserProfileComponent implements OnInit {
-
-  constructor() { }
+  
+  url: ArrayBuffer | string = '';
+  profileForm: FormGroup;
+  user: User;
+  profileId: any;
+  constructor(private sessionStorageService: SessionStorageService,
+              private profileEndpointService: ProfileEndpointService,
+              private readonly route: ActivatedRoute,
+              ) {
+    this.profileId = this.route.snapshot.paramMap.get('id');
+    this.user = sessionStorageService.getUser();
+    this.profileForm = new FormGroup({
+      profile_name:  new FormControl('', Validators.required),
+      profile_email: new FormControl(this.user.emailAddress, Validators.required),
+      profile_about: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
+    console.log('ngOnInit() is called')
+    //this.initilizationProfile();
   }
+
+  initilizationProfile() {
+    if (this.user) {
+      this.profileEndpointService.getProfileById(+this.profileId).subscribe( (response: any) =>{
+        console.log(response)
+      }, (error: HttpErrorResponse) => {
+        console.log('Error while retrieving profile')
+      });
+    }
+  }
+
+  initializationIdeas(){
+    
+  }
+
+
 
 }
